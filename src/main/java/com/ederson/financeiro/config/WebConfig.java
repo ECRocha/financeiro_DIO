a17807f1147e67fd6a8539fcb5123fd620a2ab4e
Ederson <edersonclaudiodarocha@hotmail.com>
2024-09-06 13:02:07 -0300

    Atualização para acesso em produção ajustado

--- src/main/java/com/ederson/financeiro/config/WebConfig.java
+++ src/main/java/com/ederson/financeiro/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("financeiro-app-production.up.railway.app")
+                .allowedOrigins("financeiro-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
